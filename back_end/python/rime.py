123f23789a883fabafdce0f1d96a50978010641f
Conner.Copeland <conner.copeland@umconnect.umt.edu>
2020-01-28 10:38:51 -0700

    Slightly reorganized backend, defined CLI
    Moved all C program files into a new c/ dir. Created a python/ dir and rime.py, a backend script intended to run RIME itself. Wrote tentative Command Line Interface (CLI) options

---
+++ back_end/python/rime.py
@@ -0,0 +1,27 @@
+import argparse
+import sys
+
+# create parser object, which handles commandline arguments
+def parseArgs(sysArgs):
+    parser = argparse.ArgumentParser(sysArgs)
+    # options without -- are required and positional
+    parser.add_argument("catalog", help="")
+    parser.add_argument("rip", help="")
+    parser.add_argument("output", help="")
+    # options with -- are optional and can occur in any order. If a type isn't explicitly defined, they are assumed to be booleans that are set to true when enabled
+    parser.add_argument("--ignore_warnings", "-i", action="store_true", help="Instead of stopping on warnings, ignore and continue")
+    parser.add_argument("--netcdf4", "-n", action="store_true", help="Store output in NetCDF4 file format. Any combination of output format options can be specified simultaneously")
+    parser.add_argument("--hdf5", "-hd", action="store_true", help="Store output in HDF5 file format. Any combination of output format options can be specified simultaneously")
+    parser.add_argument("--geotiff", "-g", action="store_true", help="Store output in GeoTIFF file format. Any combination of output format options can be specified simultaneously")
+    parser.add_argument("--checksum", "-c", action="store_true", help="")
+    parser.add_argument("--tar_netcdf4", "-tn", action="store_true", help="Automatically compress output NetCDF4 files. --netcdf4 must be enabled")
+    parser.add_argument("--tar_hdf5", "-th", action="store_true", help="Automatically compress output HDF5 files. --hdf5 must be enabled")
+    parser.add_argument("--tar_geotiff", "-tg", action="store_true", help="Automatically compress output GeoTIFF files. --geotiff must be enabled")
+    parser.add_argument("--tar_all", "-ta", action="store_true", help="Automatically compress output for all specified output types")
+    # example of specifying type and default value: parser.add_argument("--", type=float, default=1e-5, help="")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parseArgs(sys.argv[1:])
