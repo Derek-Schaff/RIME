0c50bcf109029015b73e0e3cca3f6a3bb1b012ae
turkishdisko <Derek.Schaff@umconnect.umt.edu>
2020-03-04 01:32:08 -0700

    Added netCDF conversion to convert.py, well at least everything I need to remember to make it work. We will need to discuss some data flow next time we meet to get it rolling cleanly. Just wanted to push it to git so I don't lose it and others can get a general idea of what's happening.

--- back_end/python/convert.py
+++ back_end/python/convert.py
@@ -2,6 +2,7 @@ import h5py
 from osgeo import gdal, gdal_array, osr
 import numpy as np
 import validate
+from ctypes import *
 
 
 # define factory pattern client
@@ -34,8 +35,49 @@ def _create_hdf5(filePath, binary, ripDic, metadataDict):
     return file
 
 
-def _create_netcdf4():
-    #TODO
+def _create_netcdf4(filePath, outputPath):
+    netCDF = CDLL("../c/netCDF.so")
+    netCDF.conv_netCDF.argtypes = [POINTER(c_int8), c_int, c_int, c_int, POINTER(c_char * 100), POINTER(c_char * 100),
+                                   c_char_p, c_char_p]
+
+    b_filePath = filePath.encode('utf-8')
+    b_outputPath = outputPath.encode('utf-8')
+    '''
+    We'll need to discuss what y'alls vision was for the conversion routine before
+    netCDF can be completed. It works now, with how I had data set up, but type conversion
+    will need to be applied once we agree on the data flow for arguments. 
+    
+    WARNING!    
+        I may or may not have half forgotten exactly why type conversions were done this way
+        but it works ??? I will update comments for why sometime.
+    
+    !!!Ctypes is a BLAST!!!
+    
+    EXAMPLES of type conversions
+    
+    #binary data read into an array
+    bin_dat = [1,2,3,4,5,6]
+    data = (c_int8 * 6)
+    dat_Arr = data(*dat)
+    
+    # Meta data dictionary will need to be split into 2 arrays, one for key(fields) and values
+        This type conversion is a janky example, it will be cleaner once we decide on how this 
+        general process will be living. This example is for just 2 instances of metadata.
+    
+    fields = (c_char * 100 * 2)()
+    fields[0].value = b'first'
+    fields[1].value = b'second'
+    vals = (c_char * 100 * 2)()
+    vals[0].value = b"1"
+    vals[1].value = b"2"
+
+    netCDF.conv_netCDF(dat_Arr, 2, 3, 2, fields, vals, b_filePath, b_outputPath)
+    #(__uint8_t *data | int data_set_rows | int data_set_cols,int meta_num | char *meta_fields[] | char *meta_vals[] |
+        char *output_path | char *log_path)
+
+    '''
+
+
     return
 
 
