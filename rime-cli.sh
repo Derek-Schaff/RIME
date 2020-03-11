96737b88c91aec1bb675a038f99c245bc1163f9b
Conner.Copeland <conner.copeland@umconnect.umt.edu>
2020-03-11 16:09:37 -0600

    pre-merge commit

--- rime-cli.sh
+++ rime-cli.sh
@@ -11,7 +11,7 @@ echo -ne '######################    (100%)\r'
 echo
 
 #echo 'docker container run -it -v ~/RIME:/usr/RIME --rm rimereqs:0.1 /bin/bash -c "clear && python3 /usr/RIME/back_end/python/rime.py $1 $2 $3"'
-sudo docker container run -it -v ~/RIME:/usr/RIME --rm rimereqs:0.1 /bin/bash -c "cd /usr/RIME/back_end/python && python3 rime.py $*"
+sudo docker container run -it -v ~/pilot_test/RIME:/usr/pilot_test/RIME:rw --rm rimereqs:0.1 /bin/bash -c "/back_end/python && python3 rime.py $*"
 
 #sudo ./x11docker --share /home/$user_name/RIME/ $container_name python3 /home/$user_name/RIME/back_end/python/rime.py
 
