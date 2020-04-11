991a6b174314c150181705c99c908e7887d81365
turkishdisko <Derek.Schaff@umconnect.umt.edu>
2020-04-10 23:01:12 -0600

    Revert "Revert "Added metadata grouping to netCDF.c""
    This reverts commit 78eba8a7

--- back_end/c/netCDF.c
+++ back_end/c/netCDF.c
@@ -68,7 +68,8 @@ int insert_meta(char *meta_vars,char *meta_vals, int ncid,int varid, int retval,
             else{
                 printf("5 %d\n",prev_id);
                 //insert attribute with prev_id as ncid
-                if(( retval = nc_put_att_text(prev_id + grp_offset,varid,token,strlen(meta_vals),meta_vals))) ERR(retval);
+                printf("prev id: %d\n",prev_id);
+                if(( retval = nc_put_att_text(prev_id,varid,token,strlen(meta_vals),meta_vals))) ERR(retval);
                 printf("var: %s value: %s\n",token,meta_vals);
             }
         }
