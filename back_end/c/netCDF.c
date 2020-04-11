78eba8a778a6091e400b7445818fee788ffaf1cc
turkishdisko <Derek.Schaff@umconnect.umt.edu>
2020-04-10 21:34:18 -0600

    Revert "Added metadata grouping to netCDF.c"
    This reverts commit 9fc75e51

--- back_end/c/netCDF.c
+++ back_end/c/netCDF.c
@@ -14,7 +14,7 @@
 
 ht_t *groups;
 
-int insert_meta(char *meta_vars,char *meta_vals, int ncid,int varid, int retval,int grp_offset){
+int insert_meta(char *meta_vars,char *meta_vals, int ncid,int varid, int retval, int grp_offset){
     char *token;
     char *string = strdup(meta_vars);
     char *dup = strdup(string);
@@ -60,9 +60,9 @@ int insert_meta(char *meta_vars,char *meta_vals, int ncid,int varid, int retval,
             }
         }
         token = strtok(NULL, "/|\0");
-    };
+    }
     free(dup);
-    return grp_offset;
+    return 0;
 }
 
 int conv_netCDF(__uint8_t *data,int data_set_rows, int data_set_cols,int meta_num, char *meta_vars[],char *meta_vals[], char *output_path, char *log_path){
@@ -95,7 +95,7 @@ int conv_netCDF(__uint8_t *data,int data_set_rows, int data_set_cols,int meta_nu
 
     /*insert meta data*/
     for(int i = 0; i < meta_num; i++){
-        grp_offset = insert_meta(meta_vars[i],meta_vals[i],ncid,NC_GLOBAL,retval,grp_offset);
+        insert_meta(meta_vars[i],meta_vals[i],ncid,NC_GLOBAL,retval,grp_offset);
     }
     printf("ESDR: %d Acqui: %d\n",ht_get(groups,"ESDR"),ht_get(groups,"AcquisitionInformation"));
     if ((retval = nc_put_var_ubyte(ncid, varid, &data[0])))
