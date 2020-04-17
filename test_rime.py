bc5acc51cae993f2c16c4ce0977c947e1146b784
Conner.Copeland <conner.copeland@umconnect.umt.edu>
2020-04-17 13:47:54 -0600

    Modifications to operation of reshape_resolution

--- test_rime.py
+++ test_rime.py
@@ -1,25 +1,64 @@
 import back_end.python.rime as rime
+import os.path as path
 import unittest
+import subprocess
 import numpy as np
 
 
+def write_uint8_to_file(path, arr):
+    with open(path, "wb") as f:
+        for i in arr:
+            f.write(i)
+
+
 class TestMethods(unittest.TestCase):
-    def write_uint8_to_file(self, path, arr):
-        with open(path, "wb") as f:
-            for i in arr:
-                f.write(i)
 
     def test_load_binary(self):
         testFilePath = "test/test_bin.bin"
         myArr = np.array([], dtype=np.uint8)
-        self.write_uint8_to_file(testFilePath, myArr)
+        write_uint8_to_file(testFilePath, myArr)
         testArr = rime.load_binary(testFilePath, "uint8")
         self.assertTrue(np.array_equal(myArr, testArr),"Numpy array written to file and Numpy array read from file should be equal")
 
+    def test_load_binary_empty(self):
+        testFilePath = "test/test_bin.bin"
         myArr = np.array([5, 4, 3, 2, 1], dtype=np.uint8)
-        self.write_uint8_to_file(testFilePath, myArr)
+        write_uint8_to_file(testFilePath, myArr)
         testArr = rime.load_binary(testFilePath, "uint8")
         self.assertTrue(np.array_equal(myArr, testArr),"Numpy array written to file and Numpy array read from file should be equal")
+
+    def test_create_output_dir(self):
+        testDir = "test/test_output"
+        rime.create_output_dir(testDir)
+
+        self.assertTrue(path.exists(testDir) and path.isdir(testDir), "Output directory test/test_dir should exist")
+
+        # cleanup dir to prevent future tests from getting confused..
+        command = "rm -r %s" % testDir
+        subprocess.run(command.split())
+
+    def test_resolution_reshape(self):
+        inputArr = np.array([1, 1, 1, 1])
+        correctArr = np.array([[1, 1], [1, 1]])
+
+        resultArr = rime.resolution_reshape(inputArr, 2, 2)
+
+        self.assertTrue(np.array_equal(correctArr, resultArr), "Input array should be reshaped to match correct array")
+
+    def test_resolution_reshape_valueerror(self):
+        inputArr = np.array([1])
+        with self.assertRaises(ValueError):
+            rime.resolution_reshape(inputArr, 2, 2)
+
+    def test_resolution_reshape_typeerror(self):
+        inputArr = []
+        with self.assertRaises(TypeError):
+            rime.resolution_reshape(inputArr, 2, 2)
+
+    def test_validate_bin_file_exists(self):
+        testPath = "test/test.bin"
+
+        self.assertTrue()
 
 
 if __name__ == '__main__':
